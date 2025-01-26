a41a58e7a486c6cef979c46d0142204bfad813e5
Marc Chen - Tour <128506536+MarcChen@users.noreply.github.com>
2025-01-26 22:16:06 +0100

    WIP : added inclusion and exlcusion filters and new website scrap - AppleCareer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from doctest import debug
 from selenium import webdriver
-from src.selenium_script import VIEJobScraper, AirFranceJobScraper, setup_driver
+from src.selenium_script import AppleJobScraper, VIEJobScraper, AirFranceJobScraper, setup_driver
 from src.notion_client import NotionClient
 from src.sms_alert import SMSAPI
 from rich.progress import Progress
@@ -21,88 +21,103 @@ if __name__ == "__main__":
     notion_client = NotionClient(NOTION_API, DATABASE_ID)
     sms_client = SMSAPI(FREE_MOBILE_USER_ID, FREE_MOBILE_API_KEY)
 
+    INCLUDE_FILTERS = ["data", "ai", "ml", "machine learning", "artificial intelligence", "big data"]
+    EXCLUDE_FILTERS = ["internship", "stage", "intern", "internship", "apprenticeship", "apprentice", "alternance"]
+
     driver = setup_driver(debug=True)
 
     url_vie = "https://mon-vie-via.businessfrance.fr/offres/recherche?query=Data"
-    scraper_vie = VIEJobScraper(url_vie, driver=driver)
+    scraper_vie = VIEJobScraper(url_vie, driver=driver, include_filters=INCLUDE_FILTERS, exclude_filters=EXCLUDE_FILTERS)
 
     url_air_france ="https://recrutement.airfrance.com/offre-de-emploi/liste-offres.aspx"
-    scraper_airfrance = AirFranceJobScraper(url = url_air_france, keyword="", contract_type="CDI", driver=driver)
+    scraper_airfrance = AirFranceJobScraper(url = url_air_france, keyword="", contract_type="CDI", driver=driver, include_filters=INCLUDE_FILTERS, exclude_filters=EXCLUDE_FILTERS)
+
+    # url_apple = "https://jobs.apple.com/fr-fr/search?sort=relevance&location=france-FRAC+singapore-SGP+hong-kong-HKGC+taiwan-TWN"
+    # scraper_apple = AppleJobScraper(url = url_apple, driver=driver, include_filters=INCLUDE_FILTERS, exclude_filters=EXCLUDE_FILTERS)
+
+
+    ### ADD THIS TO EACH SCRAPER ["data", "ai", "ml", "machine learning", "artificial intelligence", "big data"] !!!! 
+
     try:
         print("Scraping Air France offers...")
         data_Air_France = scraper_airfrance.scrape() # Scrape all offers and filter them after since it's a company website with not many offers
         print("Scraping VIE offers...")
         data_VIE = scraper_vie.scrape()
+
+        # print("Scraping Apple offers...")
+        # scraper_apple.load_all_offers()
+        # offers = scraper_apple.extract_offers()
+        # print(f"Offers found: {offers}")
     finally:
         driver.quit()
 
-    data = {
-            "total_offers": data_VIE['total_offers'] + data_Air_France['total_offers'],
-            "offers": data_VIE['offers'] + data_Air_France['offers']
-        }
-    print(f"Total offers found: {data['total_offers']}")
+    # data = {
+    #         "total_offers": data_VIE['total_offers'] + data_Air_France['total_offers'],
+    #         "offers": data_VIE['offers'] + data_Air_France['offers']
+    #     }
+    # print(f"Total offers found: {data['total_offers']}")
 
-    try:
-        with Progress() as progress:
-            task = progress.add_task("Processing job offers...", total=len(data['offers']))
-            for offer in data['offers']:
-                title = offer['Title']
+    # try:
+    #     with Progress() as progress:
+    #         task = progress.add_task("Processing job offers...", total=len(data['offers']))
+    #         for offer in data['offers']:
+    #             title = offer['Title']
 
-                if not any(keyword.lower() in title.strip().lower() for keyword in ["data", "ai", "ml", "machine learning", "artificial intelligence", "big data"]):
-                    progress.console.log(f"[blue]Job '{title}' does not contain 'data'. Skipping...[/blue]")
-                    progress.advance(task)
-                elif notion_client.title_exists(title):
-                    progress.console.log(f"[yellow]Job '{title}' already exists. Skipping...[/yellow]")
-                    progress.advance(task)
-                else:
-                    if offer['Source'] == "Business France":
-                        sms_message = (
-                            f"New VIE Job Alert!\n"
-                            f"Title: {offer['Title']}\n"
-                            f"Company: {offer['Company']}\n"
-                            f"Location: {offer['Location']}\n"
-                            f"Contract Type: {offer['Contract Type']}\n"
-                            f"Source: {offer['Source']}\n"
-                        )
-                    else:
-                        sms_message = (
-                            f"Air France Job Alert!\n"
-                            f"Title: {offer['Title']}\n"
-                            f"Job Type: {offer['Job Type']}\n"
-                            f"Job Category: {offer['Job Category']}\n"
-                            f"Location: {offer['Location']}\n"
-                            f"Contract Type: {offer['Contract Type']}\n"
-                            f"URL: {offer['URL']}\n"
-                        )
-                    sms_client.send_sms(sms_message)
-                    time.sleep(1)
+    #             if not any(keyword.lower() in title.strip().lower() for keyword in ["data", "ai", "ml", "machine learning", "artificial intelligence", "big data"]):
+    #                 progress.console.log(f"[blue]Job '{title}' does not contain 'data'. Skipping...[/blue]")
+    #                 progress.advance(task)
+    #             elif notion_client.title_exists(title):
+    #                 progress.console.log(f"[yellow]Job '{title}' already exists. Skipping...[/yellow]")
+    #                 progress.advance(task)
+    #             else:
+    #                 if offer['Source'] == "Business France":
+    #                     sms_message = (
+    #                         f"New VIE Job Alert!\n"
+    #                         f"Title: {offer['Title']}\n"
+    #                         f"Company: {offer['Company']}\n"
+    #                         f"Location: {offer['Location']}\n"
+    #                         f"Contract Type: {offer['Contract Type']}\n"
+    #                         f"Source: {offer['Source']}\n"
+    #                     )
+    #                 else:
+    #                     sms_message = (
+    #                         f"Air France Job Alert!\n"
+    #                         f"Title: {offer['Title']}\n"
+    #                         f"Job Type: {offer['Job Type']}\n"
+    #                         f"Job Category: {offer['Job Category']}\n"
+    #                         f"Location: {offer['Location']}\n"
+    #                         f"Contract Type: {offer['Contract Type']}\n"
+    #                         f"URL: {offer['URL']}\n"
+    #                     )
+    #                 sms_client.send_sms(sms_message)
+    #                 time.sleep(1)
 
-                    job_properties = {
-                        "Title": {
-                            "title": [
-                                {"type": "text", "text": {"content": offer['Title']}}
-                            ]
-                        },
-                        "Source": {"select": {"name": offer['Source']}},
-                    }
+    #                 job_properties = {
+    #                     "Title": {
+    #                         "title": [
+    #                             {"type": "text", "text": {"content": offer['Title']}}
+    #                         ]
+    #                     },
+    #                     "Source": {"select": {"name": offer['Source']}},
+    #                 }
 
-                    for field in offer:
-                        if field not in ['Title', 'Source'] and offer[field] != 'N/A':
-                            if field in ['Candidates', 'Views']:
-                                job_properties[field] = {"number": offer[field]}
-                            elif field == 'URL':
-                                job_properties[field] = {"url": offer[field]}
-                            elif field in ['Description', 'Job Type']:
-                                job_properties[field] = {
-                                    "rich_text": [
-                                        {"type": "text", "text": {"content": offer[field]}}
-                                    ]
-                                }
-                            else:
-                                job_properties[field] = {"select": {"name": offer[field]}}
-                    notion_client.create_page(job_properties)
-                    progress.console.log(f"[green]Job '{title}' added to Notion and SMS sent![/green]")
-                    progress.advance(task)
+    #                 for field in offer:
+    #                     if field not in ['Title', 'Source'] and offer[field] != 'N/A':
+    #                         if field in ['Candidates', 'Views']:
+    #                             job_properties[field] = {"number": offer[field]}
+    #                         elif field == 'URL':
+    #                             job_properties[field] = {"url": offer[field]}
+    #                         elif field in ['Description', 'Job Type']:
+    #                             job_properties[field] = {
+    #                                 "rich_text": [
+    #                                     {"type": "text", "text": {"content": offer[field]}}
+    #                                 ]
+    #                             }
+    #                         else:
+    #                             job_properties[field] = {"select": {"name": offer[field]}}
+    #                 notion_client.create_page(job_properties)
+    #                 progress.console.log(f"[green]Job '{title}' added to Notion and SMS sent![/green]")
+    #                 progress.advance(task)
             
-    except Exception as e:
-        print(f"Error processing scraped job offers: {e}")
+    # except Exception as e:
+    #     print(f"Error processing scraped job offers: {e}")
--- src/selenium_script.py
+++ src/selenium_script.py
@@ -12,7 +12,7 @@ from typing import List, Dict, Union
 import logging
 
 class JobScraperBase:
-    def __init__(self, url: str, driver: webdriver.Chrome = None):
+    def __init__(self, url: str, driver: webdriver.Chrome = None, include_filters : List[str] = None, exclude_filters : List[str] = None):
         """
         Initialize the JobScraperBase class.
 
@@ -30,8 +30,8 @@ class JobScraperBase:
             "Location": "N/A",
             "Contract Type": "N/A",
             "Duration": "N/A",
-            "Views": 0,
-            "Candidates": 0,
+            "Views": "N/A",
+            "Candidates": "N/A",
             "Source": "N/A",
             "URL": "N/A",
             "Reference": "N/A",
@@ -89,8 +89,10 @@ class JobScraperBase:
 
 
 class VIEJobScraper(JobScraperBase):
-    def __init__(self, url: str, driver: webdriver.Chrome = None):
+    def __init__(self, url: str, driver: webdriver.Chrome = None, include_filters : List[str] = None, exclude_filters : List[str] = None):
         super().__init__(url, driver=driver)
+        self.include_filters = include_filters
+        self.exclude_filters = exclude_filters
 
     def load_all_offers(self) -> None:
         """
@@ -146,16 +148,13 @@ class VIEJobScraper(JobScraperBase):
             try:
                 time.sleep(random.uniform(0.1, 0.3))  # Randomized delay per offer
                 title = offer.find_element(By.TAG_NAME, "h2").text
-                company = offer.find_element(By.CLASS_NAME, "organization").text
-                location = offer.find_element(By.CLASS_NAME, "location").text
+                if not any(keyword.lower() in title.strip().lower() for keyword in self.include_filters):
+                    print(f"Skipping offer '{title}' ...")
+                    continue
                 details = offer.find_elements(By.TAG_NAME, "li")
-                contract_type = details[0].text if len(details) > 0 else "N/A"
-                duration = details[1].text if len(details) > 1 else "N/A"
-                views = int(details[2].text.split()[0]) if len(details) > 2 else 0
-                candidates = int(details[3].text.split()[0]) if len(details) > 3 else 0
 
                 offer_data.update({
-                    "Title": offer.find_element(By.TAG_NAME, "h2").text,
+                    "Title": title,
                     "Company": offer.find_element(By.CLASS_NAME, "organization").text,
                     "Location": offer.find_element(By.CLASS_NAME, "location").text,
                     "Contract Type": details[0].text if len(details) > 0 else "N/A",
@@ -185,7 +184,7 @@ class VIEJobScraper(JobScraperBase):
             return "Unknown"
 
 class AirFranceJobScraper(JobScraperBase):
-    def __init__(self, url: str, keyword: str, contract_type: str, driver: webdriver.Chrome = None):
+    def __init__(self, url: str, keyword: str, contract_type: str, driver: webdriver.Chrome = None, include_filters : List[str] = None, exclude_filters : List[str] = None):
 
         """
         Initialize the AirFranceJobScraper class.
@@ -200,6 +199,8 @@ class AirFranceJobScraper(JobScraperBase):
         self.contract_type = contract_type
         self.offers_url = []
         self.total_offers = 0
+        self.include_filters = include_filters
+        self.exclude_filters = exclude_filters
         
     def load_all_offers(self) -> None:
         try:
@@ -241,8 +242,12 @@ class AirFranceJobScraper(JobScraperBase):
                 offers = self.driver.find_elements(By.CLASS_NAME, "ts-offer-list-item")
                 for offer in offers:
                     title_link = offer.find_element(By.CLASS_NAME, "ts-offer-list-item__title-link")
+                    title = title_link.text
+                    if not any(keyword.lower() in title.lower() for keyword in self.include_filters) or any(keyword.lower() in title.lower() for keyword in self.exclude_filters):
+                        print(f"Skipping offer '{title}' ...")
+                        continue
                     self.offers_url.append(title_link.get_attribute("href"))
-                print(f"{len(offers)} offers loaded. Total: {len(self.offers_url)}")
+                print(f"{len(offers)} offers loaded")
 
                 # Attempt to click next page
                 try:
@@ -264,6 +269,7 @@ class AirFranceJobScraper(JobScraperBase):
             raise  # Re-raise exception to see full traceback
 
         print("Finished loading all available offers.")
+        print(f"Total after filters : {len(self.offers_url)}")
 
 
     def extract_offers(self) -> List[Dict[str, Union[str, int]]]:
@@ -323,6 +329,165 @@ class AirFranceJobScraper(JobScraperBase):
         """
         return self.total_offers
     
+
+class AppleJobScraper(JobScraperBase):
+    def __init__(self, url: str, driver: webdriver.Chrome = None, include_filters : List[str] = None, exclude_filters : List[str] = None):
+
+        """
+        Initialize the AirFranceJobScraper class.
+
+        Args:
+            url (str): The URL of the job listing page to scrape.
+            keyword (str): The keyword to search for in job listings.
+            contract_type (str): The type of contract to filter job listings.
+        """
+        super().__init__(url, driver=driver)
+
+        self.offers_url = []
+        self.total_offers = 0
+        self.include_filters = include_filters
+        self.exclude_filters = exclude_filters
+        
+    def load_all_offers(self) -> None:
+        """
+        Loads all available offers from the specified URL and extracts their URLs.
+        This method navigates to the URL specified in the instance, handles the cookie consent popup if present,
+        waits for the offers to load, and then extracts the URLs of the offers from the page.
+        Raises:
+            Exception: If there is an error loading the offers or extracting the URLs.
+        Side Effects:
+            - Updates the `self.offers_url` list with the URLs of the offers.
+            - Updates the `self.total_offers` with the count of the loaded offers.
+        """
+        try:
+            self.driver.get(self.url)
+            
+            # Handle cookies - adjust selector if different on the actual page
+            try:
+                WebDriverWait(self.driver, 3).until(
+                    EC.element_to_be_clickable((By.ID, "didomi-notice-agree-button"))
+                ).click()
+                time.sleep(random.uniform(1, 2))
+            except Exception:
+                pass
+
+            # Wait for offers to load
+            WebDriverWait(self.driver, 15).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "a.table--advanced-search__title"))
+            )
+            self.total_offers = int(WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//h2[@id='resultCount']/span[1]"))
+            ).text.split()[0])
+
+            # Find all offer rows
+            offer_rows = self.driver.find_elements(By.CSS_SELECTOR, "tbody[id^='accordion_']")
+            
+            loaded_offers = 0 
+
+            while loaded_offers < self.total_offers:
+                try :
+                    # time.sleep(random.uniform(1, 2))
+                    WebDriverWait(self.driver, 15).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, "a.table--advanced-search__title"))
+                    )
+                    offer_rows = self.driver.find_elements(By.CSS_SELECTOR, "tbody[id^='accordion_']")
+                    # Extract URLs from title links
+                    for row in offer_rows:
+                        try:
+                            loaded_offers += 1
+                            title_link = row.find_element(By.CSS_SELECTOR, "a.table--advanced-search__title")
+                            job_title = title_link.text
+                            if not any(keyword.lower() in job_title.lower() for keyword in self.include_filters) or any(keyword.lower() in job_title.lower() for keyword in self.exclude_filters):
+                                print(f"Skipping offer '{job_title}' ...")
+                                continue
+                            else : 
+                                self.offers_url.append(title_link.get_attribute("href"))
+                        except Exception as e:
+                            print(f"Error extracting link from row: {str(e)}")
+
+                    print(f"Loaded {loaded_offers} / {self.total_offers}")
+
+                    # Scroll to the last offer row
+                    self.driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'center'});", offer_rows[-1])
+
+                    next_button = WebDriverWait(self.driver, 10).until(
+                        EC.element_to_be_clickable((By.CSS_SELECTOR, "li.pagination__next a:not([aria-disabled='true'])"))
+                    )
+                    self.driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'center'});", next_button)
+                    self.driver.execute_script("arguments[0].click();", next_button)
+                    time.sleep(random.uniform(1.5, 2.5))
+
+                except Exception as e:
+                    print(f"Reached last offer")
+                    break
+                
+
+        except Exception as e:
+            print(f"Error loading offers: {str(e)}")
+            raise
+
+        print(f"SCRAPED OFFERS : {self.offers_url}")
+        print(f"TOTAL OFFERS : {len(self.offers_url)}")
+        print("Finished loading all available offers.")
+
+
+    def extract_offers(self) -> List[Dict[str, Union[str, int]]]:
+        """
+        Extract offers data from the loaded page.
+
+        Returns:
+            List[Dict[str, Union[str, int]]]: A list of dictionaries containing offer details.
+        """
+        def extract_element(by, value, attribute=None, split_text=None, index=None):
+                    try:
+                        element = self.driver.find_element(by, value)
+                        text = element.get_attribute(attribute) if attribute else element.text
+                        if split_text and index is not None:
+                            return text.split(split_text)[index].strip()
+                        return text.strip()
+                    except (NoSuchElementException, IndexError, AttributeError):
+                        return "N/A"
+                    
+        offers = []
+        for offer_url in self.offers_url:
+            self.driver.get(offer_url)
+            time.sleep(random.uniform(1, 3))
+            
+            # Initialize all fields with default values
+            offer_data = self._init_offer_dict()
+
+            try:
+                offer_data.update({
+                    "Title": extract_element(By.ID, "jdPostingTitle"),
+                    "Reference": extract_element(By.ID, "jobNumber"),
+                    "Location": extract_element(By.ID, "job-location-name", split_text=",", index=0), # Retrieve City 
+                    "Schedule Type": extract_element(By.ID, "jobWeeklyHours"),
+                    "Job Type": extract_element(By.ID, "job-team-name"),
+                    "Description": "\n".join([
+                        extract_element(By.ID, "jd-job-summary"),
+                        extract_element(By.ID, "jd-description"),
+                        extract_element(By.ID, "jd-minimum-qualifications"),
+                        extract_element(By.ID, "jd-preferred-qualifications"),
+                    ]),
+                    "URL": offer_url,
+                    "Source": "Apple"
+                })
+                offers.append(offer_data)
+            except Exception as e:
+                print(f"Error extracting data for an offer: {e}")
+        return offers
+
+
+    def extract_total_offers(self) -> int:
+        """
+        Extract the total offers count displayed on the page.
+
+        Returns:
+            int: The total offers count as an integer, or 'Unknown' if an error occurs.
+        """
+        return self.total_offers
+    
+
 def setup_driver(debug : bool = False) -> webdriver.Chrome:
         """
         Set up the Selenium WebDriver with necessary options.
