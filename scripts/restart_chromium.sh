11b1de21521b4d91fe70938e1b5d0e4e84c93e7c
Marc Chen - Tour <128506536+MarcChen@users.noreply.github.com>
2025-06-27 16:54:14 +0200

    Remove obsolete Chromium launch and management scripts, along with associated unit tests

--- scripts/restart_chromium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-restart_chromium() {
-    docker stop selenium-chromium
-    docker rm selenium-chromium
-    DIR="$(dirname "$0")"
-    bash "$DIR/launch_chromium.sh"
-}
-restart_chromium
